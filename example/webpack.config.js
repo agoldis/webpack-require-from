6def0a9c56e656a0aae0d25df39667a43702d113
Andrew Goldis <agoldis@gmail.com>
2019-04-08 07:59:50 +1000

    Support web-workers + example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,21 +1,29 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const RequireFrom = require('../');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const RequireFrom = require("../");
+const path = require("path");
 
 module.exports = {
-	"output": {
-		"filename": "main.js",
-		"path": path.resolve(__dirname, 'dist/')
-	},
-	"entry": "./index.js",
-	// "mode": "development",
-	"plugins": [
-			new HtmlWebpackPlugin(),
-			new	 RequireFrom({
-				methodName: 'setSDN',
-				// path: 'customPath/',
-				replaceSrcMethodName: 'replaceDynamicSrc'
-			})
-	]
-}
-
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist/"),
+    globalObject: "this"
+  },
+  entry: ["./globals.js", "./index.js"],
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /worker\.js$/,
+        use: {
+          loader: `worker-loader`
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new RequireFrom({
+      replaceSrcMethodName: "getSrc"
+    })
+  ]
+};
