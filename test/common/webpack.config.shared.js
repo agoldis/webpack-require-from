09c3e632e1bdd5b05a554fbd0521fba06e953bd9
Andrew Goldis <agoldis@gmail.com>
2020-10-13 13:36:39 -0700

    Add webpack 5 tests

--- test/common/webpack.config.shared.js
+++ test/common/webpack.config.shared.js
@@ -1,89 +1,101 @@
-const path = require("path");
-const WebpackRequireFrom = require("../../");
+const path = require('path');
+const WebpackRequireFrom = require('../../');
 
 const pluginConfList = {
   default: {},
   empty_pluginConf: {
-    plugins: [new WebpackRequireFrom()]
+    plugins: [new WebpackRequireFrom()],
   },
   emptyObject_pluginConf: {
-    plugins: [new WebpackRequireFrom({})]
+    plugins: [new WebpackRequireFrom({})],
   },
   path_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        path: "staticPath/"
-      })
-    ]
+        path: 'staticPath/',
+      }),
+    ],
   },
   methodName_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        methodName: "getPublicPath"
-      })
-    ]
+        methodName: 'getPublicPath',
+      }),
+    ],
   },
   variableName_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        variableName: "publicPath"
-      })
-    ]
+        variableName: 'publicPath',
+      }),
+    ],
   },
   replaceSrcMethodName_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        replaceSrcMethodName: "getSrc"
-      })
-    ]
+        replaceSrcMethodName: 'getSrc',
+      }),
+    ],
   },
   replaceSrcMethodName_methodName_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        methodName: "getPublicPath",
-        replaceSrcMethodName: "getSrc"
-      })
-    ]
+        methodName: 'getPublicPath',
+        replaceSrcMethodName: 'getSrc',
+      }),
+    ],
   },
   replaceSrcMethodName_variableName_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        variableName: "publicPath",
-        replaceSrcMethodName: "getSrc"
-      })
-    ]
+        variableName: 'publicPath',
+        replaceSrcMethodName: 'getSrc',
+      }),
+    ],
   },
   replaceSrcMethodName_path_pluginConf: {
     plugins: [
       new WebpackRequireFrom({
-        path: "staticPath/",
-        replaceSrcMethodName: "getSrc"
-      })
-    ]
-  }
+        path: 'staticPath/',
+        replaceSrcMethodName: 'getSrc',
+      }),
+    ],
+  },
 };
 
-exports.buildPath = path.resolve("build");
+exports.buildPath = path.resolve('build');
 
 const baseConf = {
   entry: {
-    main: "./test/common/src/index.js"
+    main: './test/common/src/index.js',
   },
   output: {
-    filename: "[name].js",
-    chunkFilename: "[name].js",
-    publicPath: "originalPublicPath/",
-    path: exports.buildPath
-  }
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+    publicPath: 'originalPublicPath/',
+    path: exports.buildPath,
+  },
 };
 
+exports.webpack5 = {};
 exports.webpack4 = {};
 exports.webpack3 = {};
 exports.webpack2 = {};
 
 Object.entries(pluginConfList).map(([configName, configValue]) => {
+  exports.webpack5[configName] = Object.assign({}, baseConf, configValue, {
+    mode: 'development',
+    output: {
+      ...baseConf.output,
+      globalObject: 'this',
+    },
+  });
   exports.webpack4[configName] = Object.assign({}, baseConf, configValue, {
-    mode: "development"
+    mode: 'development',
+    output: {
+      ...baseConf.output,
+      globalObject: 'this',
+    },
   });
   exports.webpack3[configName] = Object.assign({}, baseConf, configValue);
   exports.webpack2[configName] = Object.assign({}, baseConf, configValue);
